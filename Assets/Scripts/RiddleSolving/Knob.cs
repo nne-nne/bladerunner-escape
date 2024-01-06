0e99ffa7e1c3656267c41babcadc463d4f966539
Adam Leczkowski <58361333+nne-nne@users.noreply.github.com>
2024-01-06 15:49:54 +0100

    knob manipulation, camera texture

--- Assets/Scripts/RiddleSolving/Knob.cs
+++ Assets/Scripts/RiddleSolving/Knob.cs
@@ -4,18 +4,59 @@ using UnityEngine;
 
 public class Knob : MonoBehaviour, ITakeable
 {
+    [SerializeField] private float value;
+    [SerializeField] private float tolerance;
+    [SerializeField] private GameObject activationMarker;
+
     public void Drop()
     {
-        throw new System.NotImplementedException();
+        Debug.Log("knob dropped");
+        activationMarker.SetActive(false);
     }
 
     public void Take()
     {
-        throw new System.NotImplementedException();
+        Debug.Log("knob taken");
+        activationMarker.SetActive(true);
     }
 
     public void Use()
     {
-        throw new System.NotImplementedException();
+        Debug.Log("knob should be used via event broadcaster");
+    }
+
+    private void Rotate(float value)
+    {
+        this.value = (this.value + value) % 360f;
+        VisualizeKnobValueChanged(this.value);
+    }
+
+    private void OnKnobValueChanged(Knob knob, float value)
+    {
+        if(knob == this)
+        {
+            Rotate(value);
+        }
+    }
+
+    private void VisualizeKnobValueChanged(float newValue)
+    {
+        transform.rotation = Quaternion.Euler(new Vector3(newValue, transform.rotation.y, transform.rotation.z));
+    }
+
+    private void Start()
+    {
+        activationMarker.SetActive(false);
+        VisualizeKnobValueChanged(value);
+    }
+
+    private void OnEnable()
+    {
+        EventBroadcaster.OnKnobValueChanged += OnKnobValueChanged;
+    }
+
+    private void OnDisable()
+    {
+        EventBroadcaster.OnKnobValueChanged -= OnKnobValueChanged;
     }
 }
