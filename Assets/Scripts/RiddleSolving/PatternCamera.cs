072226cf3bc1b6654d99bb4060d7cf77734937a6
Adam Leczkowski <58361333+nne-nne@users.noreply.github.com>
2024-01-09 11:47:09 +0100

    pattern cameras on tablet

--- Assets/Scripts/RiddleSolving/PatternCamera.cs
+++ Assets/Scripts/RiddleSolving/PatternCamera.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class PatternCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Material cameraTextureMaterial;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Material CameraTextureMaterial { get => cameraTextureMaterial; }
 }
