cc5592dffe6e0b827656aec41e4451058f3848ff
Adam Leczkowski <58361333+nne-nne@users.noreply.github.com>
2024-01-21 14:07:34 +0100

    knob moved, safe openable implemented

--- Assets/Scripts/Environment/Safe.cs
+++ Assets/Scripts/Environment/Safe.cs
@@ -2,8 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Safe : MonoBehaviour
+public class Safe : MonoBehaviour, IOpenable
 {
+    [SerializeField] private Transform doors;
+    [SerializeField] private Vector3 closedRotation;
+    [SerializeField] private Vector3 openRotation;
+    public void Close()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Open()
+    {
+        throw new System.NotImplementedException();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
