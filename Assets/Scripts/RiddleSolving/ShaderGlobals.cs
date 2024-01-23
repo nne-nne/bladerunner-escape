48527be81479445e3395b921f7a25695628d22b5
Adam Leczkowski <58361333+nne-nne@users.noreply.github.com>
2024-01-23 16:55:14 +0100

    shower riddle set up

--- Assets/Scripts/RiddleSolving/ShaderGlobals.cs
+++ Assets/Scripts/RiddleSolving/ShaderGlobals.cs
@@ -5,11 +5,25 @@ using UnityEngine;
 public class ShaderGlobals : MonoBehaviour
 {
     [SerializeField] Light mainLight;
+    [SerializeField] float bwThreshold;
 
     void Start()
     {
         Shader.SetGlobalVector("_MainLightDirection", mainLight.transform.forward);
         Shader.SetGlobalFloat("_MainLightAttenuation", mainLight.range);
+        Shader.SetGlobalFloat("_Threshold", bwThreshold);
         Shader.SetGlobalColor("_MainLightColor", mainLight.color);
+    }
+
+    public void SetBwThreshold(float value)
+    {
+        Shader.SetGlobalFloat("_Threshold", value);
+        bwThreshold = value;
+    }
+
+    public void ModifyBwThreshold(float value)
+    {
+        bwThreshold += value;
+        Shader.SetGlobalFloat("_Threshold", bwThreshold);
     }
 }
